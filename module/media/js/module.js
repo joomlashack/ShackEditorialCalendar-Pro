dcea1018ca8529d11c1bc41a3ee23c1c6f25a0ff
studieka <studieka@986210e8-35e5-42ff-90ab-28d5e4790db6>
2015-09-15 10:12:59 +0000

    activated bootstrap tooltip; added tooltip to new icons
    git-svn-id: svn://elias.pixpro.net/joomla30/sites/pixpublish/trunk@2978 986210e8-35e5-42ff-90ab-28d5e4790db6

--- module/media/js/module.js
+++ module/media/js/module.js
@@ -6,7 +6,7 @@ jQuery(function($) {
 	$.fn.newModule = function()
 	{
 		// add create new article-link
-		$(this).find('.fc-day:not(.fc-past) .pp-day-head').prepend('<div class="pp-new pp-hide" data-plugin="module"><a href="javascript:void(0)"><span class="icon-cube"></span></a></div>');
+		$(this).find('.fc-day:not(.fc-past) .pp-day-head').prepend('<div class="pp-new pp-hide" data-plugin="module"><a href="javascript:void(0)" class="hasTooltip" data-original-title="<strong>Add new</strong><br />Module"><span class="icon-cube"></span></a></div>');
 
 		return this;
 	}
