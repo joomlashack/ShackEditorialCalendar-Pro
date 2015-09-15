7c4977faa4857b5d103afa8f85cd8f7cad70624e
studieka <studieka@986210e8-35e5-42ff-90ab-28d5e4790db6>
2015-09-15 09:59:12 +0000

    updated JS; moved function from plugins to component
    git-svn-id: svn://elias.pixpro.net/joomla30/sites/pixpublish/trunk@2977 986210e8-35e5-42ff-90ab-28d5e4790db6

--- module/media/js/module.js
+++ module/media/js/module.js
@@ -7,12 +7,6 @@ jQuery(function($) {
 	{
 		// add create new article-link
 		$(this).find('.fc-day:not(.fc-past) .pp-day-head').prepend('<div class="pp-new pp-hide" data-plugin="module"><a href="javascript:void(0)"><span class="icon-cube"></span></a></div>');
-		$(this).find('.fc-day').mouseenter(function() {
-			$(this).find('.pp-new').removeClass('pp-hide');
-		})
-		.mouseleave(function() {
-			$(this).find('.pp-new').addClass('pp-hide');
-  		});
 
 		return this;
 	}
