30540edfbb8cf7055b53a0967f9678213568eb3a
tom <tom@986210e8-35e5-42ff-90ab-28d5e4790db6>
2015-09-22 14:49:28 +0000

    added multi-language for the javascript, not perfect yet
    git-svn-id: svn://elias.pixpro.net/joomla30/sites/pixpublish/trunk@3016 986210e8-35e5-42ff-90ab-28d5e4790db6

--- views/panel/view.html.php
+++ views/panel/view.html.php
@@ -24,7 +24,7 @@ class PixPublishViewPanel extends JViewLegacy
 		$doc = JFactory::getDocument();
 		// Fullcalendar
 		$doc->addScript( JUri::root().'administrator/components/com_pixpublish/media/lib/jquery/jquery-ui.custom.min.js' );
-		$doc->addScript( JUri::root().'administrator/components/com_pixpublish/media/lib/fullcalendar/fullcalendar.min.js' );
+		$doc->addScript( JUri::root().'administrator/components/com_pixpublish/media/lib/fullcalendar/fullcalendar.min.js','text/javascript',false,true );
 		$doc->addStyleSheet( JUri::root().'administrator/components/com_pixpublish/media/lib/fullcalendar/fullcalendar.css' );
 		
 		// Popup
@@ -38,7 +38,47 @@ class PixPublishViewPanel extends JViewLegacy
 		// Component
 		$doc->addScript( JUri::root().'administrator/components/com_pixpublish/media/js/pixpublish.js' );
 		$doc->addStyleSheet( JUri::root().'administrator/components/com_pixpublish/media/css/pixpublish.css' );
-		$doc->addScriptDeclaration('var addNew = "'.JText::_('COM_PIXPUBLISH_ADD_NEW').'";');
+		$doc->addScriptDeclaration('var addNew = "'.JText::_('COM_PIXPUBLISH_ADD_NEW').'";
+									var JANUARY = "'.JText::_('JANUARY').'";
+									var FEBRUARY = "'.JText::_('FEBRUARY').'";
+									var MARCH = "'.JText::_('MARCH').'";
+									var APRIL = "'.JText::_('APRIL').'";
+									var MAY = "'.JText::_('MAY').'";
+									var JUNE = "'.JText::_('JUNE').'";
+									var JULY = "'.JText::_('JULY').'";
+									var AUGUST = "'.JText::_('AUGUST').'";
+									var SEPTEMBER = "'.JText::_('SEPTEMBER').'";
+									var OCTOBER = "'.JText::_('OCTOBER').'";
+									var NOVEMBER = "'.JText::_('NOVEMBER').'";
+									var DECEMBER = "'.JText::_('DECEMBER').'";
+									var JAN = "'.JText::_('JANUARY_SHORT').'";
+									var FEB = "'.JText::_('FEBRUARY_SHORT').'";
+									var MAR = "'.JText::_('MARCH_SHORT').'";
+									var APR = "'.JText::_('APRIL_SHORT').'";
+									var MAY = "'.JText::_('MAY_SHORT').'";
+									var JUN = "'.JText::_('JUNE_SHORT').'";
+									var JUL = "'.JText::_('JULY_SHORT').'";
+									var AUG = "'.JText::_('AUGUST_SHORT').'";
+									var SEP = "'.JText::_('SEPTEMBER_SHORT').'";
+									var OCT = "'.JText::_('OCTOBER_SHORT').'";
+									var NOV = "'.JText::_('NOVEMBER_SHORT').'";
+									var DEC = "'.JText::_('DECEMBER_SHORT').'";
+									var MONDAY = "'.JText::_('MONDAY').'";
+									var TUESDAY = "'.JText::_('TUESDAY').'";
+									var WEDNESDAY = "'.JText::_('WEDNESDAY').'";
+									var THURSDAY = "'.JText::_('THURSDAY').'";
+									var FRIDAY = "'.JText::_('FRIDAY').'";
+									var SATURDAY = "'.JText::_('SATURDAY').'";
+									var SUNDAY = "'.JText::_('SUNDAY').'";
+									var MON = "'.JText::_('MON').'";
+									var TUE = "'.JText::_('TUE').'";
+									var WED = "'.JText::_('WED').'";
+									var THU = "'.JText::_('THU').'";
+									var FRI = "'.JText::_('FRI').'";
+									var SAT = "'.JText::_('SAT').'";
+									var SUN = "'.JText::_('SUN').'";
+									var TODAY = "'.JText::_('JLIB_HTML_BEHAVIOR_TODAY').'";
+									var MONTH = "'.JText::_('COM_PIXPUBLISH_MONTH').'";');
 
 		$this->addToolbar();
 		
@@ -60,12 +100,19 @@ class PixPublishViewPanel extends JViewLegacy
 	}
 	
 	/**
+
 	 * @return JDispatcher
+
 	 */
 	protected function importPlugins()
+
 	{
+
 		JPluginHelper::importPlugin( 'pixpublish' );
+
 		$dispatcher = JDispatcher::getInstance();
+
 		return $dispatcher;
+
 	}
 }
