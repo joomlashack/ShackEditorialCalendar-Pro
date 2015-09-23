9d83864082d26019698621a771a6e06bec4d36ad
studieka <studieka@986210e8-35e5-42ff-90ab-28d5e4790db6>
2015-09-23 07:58:11 +0000

    updated CSS and JS for add new icons
    git-svn-id: svn://elias.pixpro.net/joomla30/sites/pixpublish/trunk@3022 986210e8-35e5-42ff-90ab-28d5e4790db6

--- content/media/js/content.js
+++ content/media/js/content.js
@@ -6,7 +6,7 @@ jQuery(function($) {
 	$.fn.newContent = function()
 	{
 		// add create new article-link
-		$(this).find('.fc-day:not(.fc-past) .pp-day-head').prepend('<div class="pp-new pp-hide" data-plugin="content"><a href="javascript:void(0)" class="hasTooltip" data-original-title="<strong>'+addNew+'</strong><br />'+contentTypeName+'"><span class="icon-stack"></span></a></div>');
+		$(this).find('.fc-day:not(.fc-past) .pp-day-head').prepend('<div class="pp-new" data-plugin="content"><a href="javascript:void(0)" class="hasTooltip" data-original-title="<strong>'+addNew+'</strong><br />'+contentTypeName+'"><span class="icon-stack"></span></a></div>');
 
 		return this;
 	}
