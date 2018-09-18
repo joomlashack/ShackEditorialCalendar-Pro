03fd7de1672ddb8ee31689704aef85e640b1e52b
Nick Savov <nicksavov@users.noreply.github.com>
2018-09-18 14:35:52 -0500

    Add script.installer

---
+++ src/script.installer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package   ShackEditorialCalendarPro
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2018 Open Source Training, LLC. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackEditorialCalendarPro.
+ *
+ * ShackEditorialCalendarPro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackEditorialCalendarPro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackEditorialCalendarPro.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Alledia\Installer\AbstractScript;
+
+defined('_JEXEC') or die();
+
+require_once 'library/Installer/include.php';
+
+/**
+ * Custom installer script
+ */
+class Com_PixpublishInstallerScript extends AbstractScript
+{
+}
