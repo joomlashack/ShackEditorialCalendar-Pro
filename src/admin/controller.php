d3971d2be038b5295f256093766a904c9fc9897a
Bill Tomczak <bill@joomlashack.com>
2019-02-12 12:06:44 -0800

    Update file headers

--- src/admin/controller.php
+++ src/admin/controller.php
@@ -1,10 +1,25 @@
 <?php
 /**
- * @copyright       Copyright (C) 2018. All rights reserved.
- * @link            https://www.joomlashack.com/joomla-extensions/shack-editorial-calendar/
- * @author          You Rock AB 2003-2017 All Rights Reserved
- * @author          2018, Joomlashack <help@joomlashack.com> - https://www.joomlashack.com.
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @package   ShackEditorialCalendar-Pro
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @author    2003-2017 You Rock AB. All Rights Reserved
+ * @copyright 2018-2019 Joomlashack.com. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackEditorialCalendar-Pro.
+ *
+ * ShackEditorialCalendar-Pro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackEditorialCalendar-Pro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackEditorialCalendar-Pro.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // No direct access.
