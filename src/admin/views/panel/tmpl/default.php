56e591d7870cd9b31b2a3b8a6b2f57b34f112827
Bill Tomczak <bill@joomlashack.com>
2019-07-10 15:01:32 -0700

    Clean up template code

--- src/admin/views/panel/tmpl/default.php
+++ src/admin/views/panel/tmpl/default.php
@@ -29,39 +29,31 @@ JHtml::_('formbehavior.chosen', 'select');
 JHTML::_('behavior.modal');
 
 $base_url = JRoute::_('index.php?option=com_pixpublish&format=json', false);
-$ed       = JFactory::getEditor();
+
+$mainClass = empty($this->siderbar) ? '' : 'span10';
+
+if (!empty($this->sidebar)) :
+    ?>
+    <div id="j-sidebar-container" class="span2">
+        <form id="pixpublish_search" method="POST" onsubmit=""
+              action="javascript:(function($) { $('#calendar').fullCalendar( 'refetchEvents' ); }(jQuery));">
+            <?php echo $this->sidebar; ?>
+        </form>
+    </div>
+<?php
+endif;
 ?>
-<script type="text/javascript">
-    <!--
-
-    //-->
-</script>
-<?php if (!empty($this->sidebar)) : ?>
-<div id="j-sidebar-container" class="span2">
-    <form id="pixpublish_search" method="POST" onsubmit=""
-          action="javascript:(function($) { $('#calendar').fullCalendar( 'refetchEvents' ); }(jQuery));">
-        <?php echo $this->sidebar; ?>
-    </form>
-</div>
-<div id="j-main-container" class="span10">
-    <?php else : ?>
-    <div id="j-main-container">
-        <?php endif; ?>
-        <?php if (count($this->infotexts) > 0): ?>
-            <div>
-                <?php foreach ($this->infotexts as $row): ?>
-                    <?php echo $row; ?>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-        <div id='calendar' style='margin:3em 0;font-size:13px' data-base-url="<?php echo $base_url; ?>">
+<div id="j-main-container" class="<?php echo $mainClass; ?>">
+    <?php
+    if (count($this->infotexts) > 0) : ?>
+        <div>
+            <?php
+            foreach ($this->infotexts as $row) :
+                echo $row;
+            endforeach;
+            ?>
         </div>
+    <?php endif; ?>
+    <div id='calendar' style='margin:3em 0;font-size:13px' data-base-url="<?php echo $base_url; ?>">
     </div>
-    <div style="display: none;" id="sudde">
-        <?php
-        if ($ed->get('_name') != 'jce') {
-            echo $ed->display('content_sudde', '', '0', '0', '0', '0', false);
-        }
-        ?>
-    </div>
-
+</div>
